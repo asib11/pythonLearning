33731552f1daec84f639257b4a4a5adca9d03979
asib <ahmedasib1@gmail.com>
2022-05-01 23:17:07 +0600

    multipule conditions

--- secondcode.py
+++ secondcode.py
@@ -1,6 +1,15 @@
-name = input("what is your name?")
-print("my name is "+ name)
+#name = input("what is your name?")
+#print("my name is "+ name)
 
 print("what is your name?")
 name2 = input()
 print("my name is " + name2)
+
+if (name2 == "dada"):
+    print ("this is dada")
+elif (name2 == "abir"):
+    print ("this is muta")
+elif (name2 == "opi"):
+    print("this is cheater")
+else:
+    print ("wrong input")
