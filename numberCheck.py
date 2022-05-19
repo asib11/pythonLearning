cf0025709cc0f1cfb935332a6641637ed48ba0cd
asib <ahmedasib1@gmail.com>
2022-05-19 12:06:49 +0600

    casting modify for effencienty

---
+++ numberCheck.py
@@ -0,0 +1,9 @@
+number = input("input number : ")
+number = int(number)
+#print(number)
+if (number > 0):
+    print(f"{number} is positive number")
+elif number == 0:
+    print ("this is 0")
+else:
+    print(f"{number} is negative")
