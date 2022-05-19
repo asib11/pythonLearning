8da6789816f5c32a28ad0a642e0edd971e798745
asib <ahmedasib1@gmail.com>
2022-05-19 23:32:49 +0600

    added nested conditions

--- numberCheck.py
+++ numberCheck.py
@@ -1,9 +1,12 @@
 number = input("input number : ")
-number = int(number)
-#print(number)
-if (number > 0):
-    print(f"{number} is positive number")
-elif number == 0:
-    print ("this is 0")
+
+if number :
+    number = int(number)
+    if (number > 0):
+        print(f"{number} is positive number")
+    elif number == 0:
+        print ("this is 0")
+    else:
+        print(f"{number} is negative")
 else:
-    print(f"{number} is negative")
+    print("enter the value")
