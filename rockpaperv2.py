5ffc031a35f39b1570044d82b6ef41edb5ce3fd8
asib <ahmedasib1@gmail.com>
2022-05-21 18:18:09 +0600

    hide user input so that player can not see other input

--- rockpaperv2.py
+++ rockpaperv2.py
@@ -1,25 +1,29 @@
+import getpass
+
 print("*rock* \n*paper* \n*scissors*")
-choice = input("player one : ")
-choice2 = input ("player two: ")
+
+choice = getpass.getpass ( "player one : ")
+choice2 = getpass.getpass ( "player two: ")
 print("Shoot!")
+print("player 1's move : " + choice)
+print("player 2's move : " + choice2)
+
 if choice == choice2 :
-    print("2players are tie")
-elif choice or choice2 :
-    if choice == "rock" :
-        if choice2 == "scissors":
-            print("1st player win")
-        else :    
-            print("2nd player win")
-    elif choice == "paper" :
-        if choice2 == "rock":
-            print("1st player win")
-        else :
-             print("2nd player win")
-    else:                                   #this condition belongs to scissors vs rest two
-    #elif choice == "scissors" :
-        if choice2 == "paper" :
-            print("1st player win")
-        else :
-             print("2nd player win")
+    print("2players are tie") 
+elif choice == "rock" :
+    if choice2 == "scissors":
+        print("1st player win")
+    elif choice2 =="paper" :    
+        print("2nd player win")
+elif choice == "paper" :
+    if choice2 == "rock":
+        print("1st player win")
+    elif choice2 == "scissors" :
+         print("2nd player win")                                  
+elif choice == "scissors" :
+    if choice2 == "paper" :
+        print("1st player win")
+    elif choice2 == "rock" :
+         print("2nd player win")
 else : 
     print("enter valid input")
