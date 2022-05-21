cbfa034bac9a67812a90037624c58a07234073c7
asib <ahmedasib1@gmail.com>
2022-05-21 18:30:25 +0600

    player vs computer modify some feature

--- rockpaperv3.py
+++ rockpaperv3.py
@@ -2,7 +2,8 @@ import random
 import getpass
 
 print("*rock* \n*paper* \n*scissors*")
-choice = getpass.getpass("player one move (hidden input): ").lower()
+name = input("your name : ")
+choice = getpass.getpass(f"{name}'s move (hidden input): ").lower()
 random_num = random.randint(1,3)
 if random_num == 1:
     computer = "rock"
@@ -11,7 +12,7 @@ elif random_num == 2 :
 else :
     computer = "scissors"
 
-print("player's move : " + choice)
+print(f"{name}'s move : " + choice)
 print("computer's move : "+ computer)
 print("Shoot!")
 
@@ -19,17 +20,17 @@ if choice == computer :
     print("two players are tie")
 elif choice == "rock" :
     if computer == "scissors":
-        print("1st player win")
+        print(f"{name}'s player win")
     else : 
         print("computer player win")
 elif choice == "paper" :
     if computer == "rock":
-        print("1st player win")
+        print(f"{name}'s player win")
     else :
          print("computer player win")
 elif choice == "scissors":                                 
     if computer == "paper" :
-        print("1st player win")
+        print(f"{name}'s player win")
     else :
          print("computer player win")
 else : 
