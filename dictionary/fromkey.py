6a5dd1314e740d3bf3a81e73a165690f4c2d947c
asib <ahmedasib1@gmail.com>
2022-08-21 23:15:03 +0600

    add fromkey

--- dictionary/fromkey.py
+++ dictionary/fromkey.py
@@ -1,3 +1,8 @@
 item = {'name':'asib', 'id':31, 'section':2,'intake':40}
 item2 = item.copy()
 print(f"item2 is {item2}")
+
+fromkey_make = {}.fromkeys(['name','id','intake'],'none')
+print(fromkey_make)
+fromkey_make2 = dict().fromkeys('abcd','unknown') #if can not use list, each of letter is iterable
+print(fromkey_make2)
