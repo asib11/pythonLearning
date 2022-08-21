3e7adadff6586939b9d902136e9d14f7b2a4ded3
asib <ahmedasib1@gmail.com>
2022-08-21 23:48:12 +0600

    add some method

--- dictionary/fromkey.py
+++ dictionary/fromkey.py
@@ -1,8 +1,18 @@
-item = {'name':'asib', 'id':31, 'section':2,'intake':40}
+item = {'name': 'asib', 'id': 31, 'section': 2, 'intake': 40}
 item2 = item.copy()
 print(f"item2 is {item2}")
 
-fromkey_make = {}.fromkeys(['name','id','intake'],'none')
+fromkey_make = {}.fromkeys(['name', 'id', 'intake'], 'none')
 print(fromkey_make)
-fromkey_make2 = dict().fromkeys('abcd','unknown') #if can not use list, each of letter is iterable
+# if can not use list, each of letter is iterable
+fromkey_make2 = dict().fromkeys('abcd', 'unknown')
 print(fromkey_make2)
+
+# if we use get it does not show error if there is no existed key
+
+print(item.get('name'))
+
+if item.get('unversity name') == None:
+    print("not found")
+
+print(f'output: {item.clear()}')
