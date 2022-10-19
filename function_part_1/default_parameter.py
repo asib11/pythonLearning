d2af40cf799197ebfaea909caf52c4f403d1ac38
asib <ahmedasib1@gmail.com>
2022-10-19 09:52:25 +0600

    spotify and default parameter

--- function_part_1/default_parameter.py
+++ function_part_1/default_parameter.py
@@ -4,13 +4,24 @@ def name(first='oh',last = 'no'):
 print(name())
 print(name(last ='ahmed',first ='asib'))
 
-x = input('your 1st input: ')
-y = input('your 2nd input: ')
+#x = input('your 1st input: ')
+#y = input('your 2nd input: ')
 
-print(name(y,x))
+#print(name(y,x))
 
 def power(num,power=2):
     return num**power
 
 print(power(4,5))
 print(power(5))
+
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def my_create(a,b,fn=add):
+    return fn(a,b)
+
+print(my_create(2,3,subtract))
---
+++ spotify.py
@@ -0,0 +1,16 @@
+playlist = {
+  'title': 'like song',
+  'author': 'asib',
+  'songs':[
+    {'title':'let me down','artist':['mc','pitbull'],'duration':2.5},
+    {'title':'paper cut','artist':['linkin park'],'duration':5.5},
+    {'title':'alone','artist':['masmello'],'duration':4}
+  ]  
+}
+
+total_duration = 0
+print(playlist['songs'])
+print(playlist['title'])
+for song in playlist['songs']:
+    total_duration += song['duration']
+print(total_duration)
