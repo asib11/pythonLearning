9562388046520622756ed79e66da8eba2d8e88d9
asib <ahmedasib1@gmail.com>
2022-10-26 23:43:32 +0600

    lambda function map and filter

---
+++ lambda/filter.py
@@ -0,0 +1,3 @@
+name = ['asib','ahmed', 'topu', 'opu', 'akash']
+name_filter = list(map(lambda name: f'all name\'s start with a is {name} ',filter(lambda x: x[0] == 'a',name)))
+print(name_filter)
---
+++ lambda/lambda.py
@@ -0,0 +1,7 @@
+sum = lambda a,b : a+b
+print(sum(20,10))
+
+prema = [1,2,4,5,3]
+prema2 = list(map(lambda n : n**2 , prema))  #iteratable and its once iterate
+print(prema2)
+
