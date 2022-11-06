08503d0c9ed7fbb5419e4ed188b8969996b0f30c
asib <ahmedasib1@gmail.com>
2022-11-06 10:16:37 +0600

    modify lambda

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -5,3 +5,6 @@ prema = [1,2,4,5,3]
 prema2 = list(map(lambda n : n**2 , prema))  #iteratable and its once iterate
 print(prema2)
 
+some_list = ['asib','badhon','opu']
+print([l[0]== 'a' for l in some_list])
+
