1d14f08b92d5836cd9b4057d7a087c5aa5ea195d
asib <ahmedasib1@gmail.com>
2022-11-16 16:18:47 +0600

    caeser cipher decryption added

--- cryptography/caesar_cipher.py
+++ cryptography/caesar_cipher.py
@@ -1,13 +1,24 @@
-text = input("enter a string :")
-key =int(input("key number :"))
-ascii_values = [ord(character) for character in text]
-encrypted=[]
-
-for i in ascii_values:
+text = input('input a string: ')
+key = int(input('input a key: '))
+convert_ascii = []
+encrypted = []
+for c in text:
+    convert_ascii.append(ord(c)) 
+#print(convert_ascii) my name is asib
+for i in convert_ascii:
     if i==32:
         encrypted += chr(i)
-    elif i>=65 and i<=90:
-        encrypted += [chr(((i)+key-65)% 26+65)]
+    elif i>=65 or i<=90:
+        encrypted += chr(((i)+key-65)%26+65)
+    elif i>=97 or i<=122
+        encrypted += chr(((i)+key-97)%26+97)
+print(encrypted)
+print(f"encrypted: {''.join(encrypted)}")
+
+decrypted = []
+for j in encrypted:
+    if j==' ':
+        decrypted +=j
     else:
-       encrypted += [chr(((i)+key-97)% 26+97)] 
-print(''.join(encrypted)) 
+        decrypted += chr((ord(j)-key-97)%26+97)
+print(f"decrypted: {''.join(decrypted)}")
