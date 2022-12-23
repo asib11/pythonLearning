52f6d22866270e12122457ce0023c865b1ee5a50
asib <ahmedasib1@gmail.com>
2022-12-23 23:46:50 +0600

    zip modified

---
+++ lambda/zip.py
@@ -0,0 +1,7 @@
+name = ['asib','hamza','tanveer']
+mid = [27,21,24]
+final = [35,30,32]
+out_of_30 =[27,24,23]
+
+print(dict(zip(name,map(lambda s: s[0]+s[1]+s[2],zip(mid,final,out_of_30)))))
+print({t[0]: t[1]+t[2]+t[3] for t in zip(name,mid,final,out_of_30)})
