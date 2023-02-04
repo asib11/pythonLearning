05549e92ac11ab00fc8f9d9c3e70666a7e5814f8
asib <ahmedasib1@gmail.com>
2023-02-04 23:41:36 +0600

    request used

--- http/first.py
+++ http/first.py
@@ -1,7 +1,9 @@
 import requests
 
-url = 'https://286545066348-j31et1vt78arg8ii5voun5qpqn252lq1.apps.googleusercontent.com'
-res = requests.post(url, auth=('asibahmed4@gmail.com', '27asib42'))
+url = 'https://286545066348-pnpisu8ioi7lq0jlvfaormnr8cjafqd1.apps.googleusercontent.com'
+res = requests.post(url, auth=('asibahmed4@gmail.com'))
+print(res.status_code)
+
 if res.status_code == 200:
     print('ok')
 else:
