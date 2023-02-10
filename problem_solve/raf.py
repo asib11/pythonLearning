590667c03f6b644c7618634f19046ddee2515c61
asib <ahmedasib1@gmail.com>
2023-02-10 21:58:53 +0600

    modifiy

--- problem_solve/raf.py
+++ problem_solve/raf.py
@@ -5,9 +5,20 @@ if __name__ == '__main__':
         score = float(input())
         a.append([name, score])
         a.sort(key = lambda u: u[1])
+    b ={a}
     for i in range(len(a)):
         if a[1][1] == a[i][1]:
             a.sort()#(key= lambda u: u[0])
             print(a[i][0])
         
     #print(a[1][0])
+
+# 4
+# Rachel
+# -50
+# Mawer
+# -50
+# Sheen
+# -50
+# Shaheen
+# 51
